9d18e3a0aa374783518ec9757f5fea5ee416ba52
whit33y <troppout@gmail.com>
2025-01-25 17:59:58 +0100

    add generated starting-page component

---
+++ src/app/pages/starting-page/starting-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starting-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './starting-page.component.html',
+  styleUrl: './starting-page.component.scss'
+})
+export class StartingPageComponent {
+
+}
