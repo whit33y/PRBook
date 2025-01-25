395bab506d51e316a45abd2b1a96691a84333aa4
whit33y <troppout@gmail.com>
2025-01-25 19:12:01 +0100

    add first route and some styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StartingPageComponent } from './pages/starting-page/starting-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'welcome',
+    component: StartingPageComponent,
+    title: 'Welcome to PRBook',
+  },
+];
