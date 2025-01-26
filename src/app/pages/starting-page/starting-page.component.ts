ee8fcc27694521610980a211bc36e191e2f9c9a2
whit33y <troppout@gmail.com>
2025-01-26 14:12:00 +0100

    add responsivnes and content to starting page

--- src/app/pages/starting-page/starting-page.component.ts
+++ src/app/pages/starting-page/starting-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/elements/button/button.component';
 
 @Component({
   selector: 'app-starting-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './starting-page.component.html',
-  styleUrl: './starting-page.component.scss'
+  styleUrl: './starting-page.component.scss',
 })
-export class StartingPageComponent {
-
-}
+export class StartingPageComponent {}
