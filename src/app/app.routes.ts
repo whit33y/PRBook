755e29d758b5a7ec99da123923431893d7cbe52b
Jakub Bryska <jakub.bryska@daw-systems.pl>
2025-01-29 09:06:57 +0100

    add home-page component + routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StartingPageComponent } from './pages/starting-page/starting-page.component';
 import { LoginPageComponent } from './pages/login/login-page/login-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: 'login',
     component: LoginPageComponent,
     title: 'PRBook login',
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    title: 'PRBook- home',
   },
 ];
