3b0a5bdab2b2366b06f8aa3e50a3e3c7c9dc8311
whit33y <troppout@gmail.com>
2025-02-02 18:55:38 +0100

    add validators on register form

--- src/app/pages/login/login-page/login-page.component.ts
+++ src/app/pages/login/login-page/login-page.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/elements/button/button.component';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth-service';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -15,10 +20,18 @@ export class LoginPageComponent {
   register: boolean = false;
   forgot: boolean = false;
   loading: boolean = false;
+  error: string = '';
   registerForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-    name: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
   constructor(private router: Router, private authService: AuthService) {}
@@ -44,22 +57,32 @@ export class LoginPageComponent {
 
   sendRegister() {
     this.loading = true;
-    setTimeout(() => {
-      this.authService
-        .register(
-          this.registerForm.value.email!,
-          this.registerForm.value.password!,
-          this.registerForm.value.name!
-        )
-        .then(() => {
-          this.loading = false;
-          console.log('Successfully registered!');
-        })
-        .catch((error) => {
-          this.loading = false;
-          console.error('Something went wrong during registration: ', error);
-        });
-    }, 2000);
+    if (
+      this.registerForm.value.password ===
+        this.registerForm.value.confirmPassword &&
+      this.registerForm.valid
+    ) {
+      setTimeout(() => {
+        this.authService
+          .register(
+            this.registerForm.value.email!,
+            this.registerForm.value.password!,
+            this.registerForm.value.name!
+          )
+          .then(() => {
+            this.loading = false;
+            console.log('Successfully registered!');
+          })
+          .catch((error) => {
+            this.loading = false;
+            console.error('Something went wrong during registration: ', error);
+          });
+      }, 2000);
+    } else {
+      this.loading = false;
+      this.error = 'Please fix the errors before submitting.';
+      return;
+    }
   }
 
   sendLogout() {
