340ef122009f98cbbc03356ee55126879a918fff
whit33y <troppout@gmail.com>
2025-02-02 14:11:16 +0100

    add registering function/ testing appwrite

--- src/app/pages/login/login-page/login-page.component.ts
+++ src/app/pages/login/login-page/login-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { InputComponent } from '../../../components/elements/input/input.component';
 import { ButtonComponent } from '../../../components/elements/button/button.component';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
@@ -14,7 +15,32 @@ export class LoginPageComponent {
   register: boolean = false;
   forgot: boolean = false;
 
-  constructor(private router: Router) {}
+  registerForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    name: new FormControl(''),
+  });
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  sendLogin() {
+    this.authService.login(
+      this.registerForm.value.email!,
+      this.registerForm.value.password!
+    );
+  }
+
+  sendRegister() {
+    this.authService.register(
+      this.registerForm.value.email!,
+      this.registerForm.value.password!,
+      this.registerForm.value.name!
+    );
+  }
+
+  sendLogout() {
+    this.authService.logout();
+  }
 
   navigateToHome() {
     console.log('home');
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,10 +1,43 @@
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { ID, Account } from 'appwrite';
+import { client } from '../../lib/appwrite';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private account: Account;
+  private loggedInUserSubject = new BehaviorSubject<any>(null);
+  loggedInUser$ = this.loggedInUserSubject.asObservable();
+  constructor() {
+    this.account = new Account(client);
+    this.checkCurrentSession();
+  }
+
+  async login(email: string, password: string) {
+    await this.account.createEmailPasswordSession(email, password);
+    const user = await this.account.get();
+    this.loggedInUserSubject.next(user);
+  }
+
+  async register(email: string, password: string, name: string) {
+    await this.account.create(ID.unique(), email, password, name);
+    return this.login(email, password);
+  }
+
+  async logout() {
+    await this.account.deleteSession('current');
+    this.loggedInUserSubject.next(null);
+  }
+
+  private async checkCurrentSession() {
+    try {
+      const user = await this.account.get();
+      this.loggedInUserSubject.next(user);
+    } catch (error) {
+      this.loggedInUserSubject.next(null);
+    }
+  }
 }
 
 export type User = {
