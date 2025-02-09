2d6774dd79debac032b4c58ad8eeaebd84edaa57
whit33y <troppout@gmail.com>
2025-02-09 13:34:24 +0100

    add pr history page component

---
+++ src/app/pages/pr-history-page/pr-history-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pr-history-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pr-history-page.component.html',
+  styleUrl: './pr-history-page.component.scss'
+})
+export class PrHistoryPageComponent {
+
+}
