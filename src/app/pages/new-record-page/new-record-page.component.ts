92026c03d9e71989662de839babc983647d25f43
whit33y <troppout@gmail.com>
2025-02-16 19:06:15 +0100

    add new-record page component

---
+++ src/app/pages/new-record-page/new-record-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-record-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-record-page.component.html',
+  styleUrl: './new-record-page.component.scss'
+})
+export class NewRecordPageComponent {
+
+}
