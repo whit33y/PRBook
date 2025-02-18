c9987cde3971ca44af7c0c0a18b3bd30002d97c2
whit33y <troppout@gmail.com>
2025-02-18 19:23:39 +0100

    add subscribe to query params on new record page

--- src/app/pages/new-record-page/new-record-page.component.ts
+++ src/app/pages/new-record-page/new-record-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-new-record-page',
   standalone: true,
   imports: [],
   templateUrl: './new-record-page.component.html',
-  styleUrl: './new-record-page.component.scss'
+  styleUrl: './new-record-page.component.scss',
 })
 export class NewRecordPageComponent {
+  constructor(private route: ActivatedRoute) {}
 
+  params?: Params;
+  type?: string;
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.params = params;
+      this.type = this.params['type'];
+    });
+  }
 }
