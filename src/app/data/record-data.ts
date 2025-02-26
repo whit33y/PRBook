567e29f4a628be93beb21b249076b71c0f8dbb29
whit33y <troppout@gmail.com>
2025-02-26 19:45:12 +0100

    change separated data to another file

---
+++ src/app/data/record-data.ts
@@ -0,0 +1,68 @@
+export const runningDistances = [0.1, 0.4, 1, 2, 3, 5, 10, 15, 21, 42, 50, 100];
+export const cyclingDistances = [10, 20, 40, 50, 90, 100, 160, 200];
+export const swimmingDistances = [0.1, 0.2, 0.4, 0.8, 1.5, 3.8, 5, 10];
+
+export const bodyPart = ['Chest', 'Back', 'Legs', 'Shoulders', 'Arms', 'Core'];
+
+export const bodyPartExercises = {
+  Chest: [
+    'Bench Press',
+    'Incline Bench Press',
+    'Decline Bench Press',
+    'Dumbbell Fly',
+    'Chest Dips',
+    'Push-ups',
+    'Cable Crossover',
+    'Pec Deck Machine',
+  ],
+  Back: [
+    'Pull-ups',
+    'Lat Pulldown',
+    'Barbell Row',
+    'Dumbbell Row',
+    'Deadlift',
+    'Face Pulls',
+    'T-Bar Row',
+    'Seated Cable Row',
+  ],
+  Legs: [
+    'Squat',
+    'Leg Press',
+    'Lunges',
+    'Romanian Deadlift',
+    'Leg Curl',
+    'Leg Extension',
+    'Calf Raise',
+    'Bulgarian Split Squat',
+  ],
+  Shoulders: [
+    'Overhead Press',
+    'Dumbbell Shoulder Press',
+    'Arnold Press',
+    'Lateral Raises',
+    'Front Raises',
+    'Face Pulls',
+    'Reverse Fly',
+    'Shrugs',
+  ],
+  Arms: [
+    'Bicep Curl',
+    'Hammer Curl',
+    'Preacher Curl',
+    'Tricep Dips',
+    'Tricep Pushdown',
+    'Overhead Tricep Extension',
+    'Close-Grip Bench Press',
+    'Reverse Curl',
+  ],
+  Core: [
+    'Plank',
+    'Crunches',
+    'Russian Twists',
+    'Leg Raises',
+    'Hanging Leg Raises',
+    'Ab Rollout',
+    'Bicycle Crunches',
+    'Dead Bug',
+  ],
+};
--- src/app/pages/new-record-page/new-record-page.component.ts
+++ src/app/pages/new-record-page/new-record-page.component.ts
@@ -9,6 +9,13 @@ import {
   Validators,
 } from '@angular/forms';
 import { ButtonComponent } from '../../components/elements/button/button.component';
+import {
+  bodyPart,
+  bodyPartExercises,
+  cyclingDistances,
+  runningDistances,
+  swimmingDistances,
+} from '../../data/record-data';
 
 @Component({
   selector: 'app-new-record-page',
@@ -19,6 +26,12 @@ import { ButtonComponent } from '../../components/elements/button/button.compone
 })
 export class NewRecordPageComponent {
   user: any;
+  runningDistances = runningDistances;
+  cyclingDistances = cyclingDistances;
+  swimmingDistances = swimmingDistances;
+  bodyPart = bodyPart;
+  bodyPartExercises = bodyPartExercises;
+
   constructor(
     private route: ActivatedRoute,
     private AppwriteDbService: AppwriteDbService,
@@ -57,10 +70,6 @@ export class NewRecordPageComponent {
     });
   }
 
-  runningDistances = [0.1, 0.4, 1, 2, 3, 5, 10, 15, 21, 42, 50, 100];
-  cyclingDistances = [10, 20, 40, 50, 90, 100, 160, 200];
-  swimmingDistances = [0.1, 0.2, 0.4, 0.8, 1.5, 3.8, 5, 10];
-
   newRecord = new FormGroup({
     type: new FormControl('run'),
     distance: new FormControl(0.1),
@@ -76,71 +85,6 @@ export class NewRecordPageComponent {
     excercise: new FormControl('Bench Press'),
     reps: new FormControl(1),
   });
-
-  bodyPart = ['Chest', 'Back', 'Legs', 'Shoulders', 'Arms', 'Core'];
-
-  bodyPartExercises = {
-    Chest: [
-      'Bench Press',
-      'Incline Bench Press',
-      'Decline Bench Press',
-      'Dumbbell Fly',
-      'Chest Dips',
-      'Push-ups',
-      'Cable Crossover',
-      'Pec Deck Machine',
-    ],
-    Back: [
-      'Pull-ups',
-      'Lat Pulldown',
-      'Barbell Row',
-      'Dumbbell Row',
-      'Deadlift',
-      'Face Pulls',
-      'T-Bar Row',
-      'Seated Cable Row',
-    ],
-    Legs: [
-      'Squat',
-      'Leg Press',
-      'Lunges',
-      'Romanian Deadlift',
-      'Leg Curl',
-      'Leg Extension',
-      'Calf Raise',
-      'Bulgarian Split Squat',
-    ],
-    Shoulders: [
-      'Overhead Press',
-      'Dumbbell Shoulder Press',
-      'Arnold Press',
-      'Lateral Raises',
-      'Front Raises',
-      'Face Pulls',
-      'Reverse Fly',
-      'Shrugs',
-    ],
-    Arms: [
-      'Bicep Curl',
-      'Hammer Curl',
-      'Preacher Curl',
-      'Tricep Dips',
-      'Tricep Pushdown',
-      'Overhead Tricep Extension',
-      'Close-Grip Bench Press',
-      'Reverse Curl',
-    ],
-    Core: [
-      'Plank',
-      'Crunches',
-      'Russian Twists',
-      'Leg Raises',
-      'Hanging Leg Raises',
-      'Ab Rollout',
-      'Bicycle Crunches',
-      'Dead Bug',
-    ],
-  };
 
   validateTimeInput(event: any): void {
     let input = event.target.value;
