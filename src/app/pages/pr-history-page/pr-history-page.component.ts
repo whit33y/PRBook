8f1935fcc5f1f19d6796c27cbc15460f5d7f8b7f
Jakub Bryska <jakub.bryska@daw-systems.pl>
2025-02-27 12:32:03 +0100

    add basic requests on pr history page

--- src/app/pages/pr-history-page/pr-history-page.component.ts
+++ src/app/pages/pr-history-page/pr-history-page.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { AppwriteDbService } from '../../services/appwrite-db.service';
+import { AuthService } from '../../services/auth-service';
+import {
+  GymRecordsDocuments,
+  RunningAndCyclingRecordsDocuments,
+} from '../../services/interfaces/appwrite-db.interfaces';
 
 @Component({
   selector: 'app-pr-history-page',
   standalone: true,
   imports: [],
   templateUrl: './pr-history-page.component.html',
-  styleUrl: './pr-history-page.component.scss'
+  styleUrl: './pr-history-page.component.scss',
 })
 export class PrHistoryPageComponent {
+  user: any;
+  constructor(
+    private appWriteDbService: AppwriteDbService,
+    private authService: AuthService
+  ) {
+    this.authService.loggedInUser$.subscribe((user) => {
+      this.user = user;
+    });
+  }
 
+  enduranceRecords: RunningAndCyclingRecordsDocuments[] = [];
+  getUserEnduranceRecords(user_id: string) {
+    this.appWriteDbService.getUserRecords(user_id).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Completed.');
+      },
+    });
+  }
+
+  gymRecords: GymRecordsDocuments[] = [];
+  getUserGymRecords(user_id: string) {
+    this.appWriteDbService.getUserGymRecords(user_id).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+      complete: () => {
+        console.log('Completed.');
+      },
+    });
+  }
 }
