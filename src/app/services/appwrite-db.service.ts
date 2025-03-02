247a27a2d15dc1817551f886323ad9aa4943644c
whit33y <troppout@gmail.com>
2025-03-02 12:55:47 +0100

    change request to get desc sorted data

--- src/app/services/appwrite-db.service.ts
+++ src/app/services/appwrite-db.service.ts
@@ -33,7 +33,7 @@ export class AppwriteDbService {
       this.database.listDocuments(
         this.databaseId,
         this.runningAndCyclingRecordsId,
-        [Query.equal('user_id', userId)]
+        [Query.equal('user_id', userId), Query.orderDesc('$createdAt')]
       )
     ).pipe(
       map(
@@ -53,6 +53,7 @@ export class AppwriteDbService {
     return from(
       this.database.listDocuments(this.databaseId, this.gymRecordsId, [
         Query.equal('user_id', userId),
+        Query.orderDesc('$createdAt'),
       ])
     ).pipe(
       map((response) => response.documents as GymRecordsDocuments[]),
@@ -63,7 +64,10 @@ export class AppwriteDbService {
     );
   }
 
-  getBestGymRecord(userId: string, exercise: string): Observable<GymRecordsDocuments | null> {
+  getBestGymRecord(
+    userId: string,
+    exercise: string
+  ): Observable<GymRecordsDocuments | null> {
     if (!userId || !exercise) {
       return of(null);
     }
@@ -75,28 +79,44 @@ export class AppwriteDbService {
         Query.limit(1),
       ])
     ).pipe(
-      map((response) => (response.documents.length > 0 ? response.documents[0] as GymRecordsDocuments : null)),
+      map((response) =>
+        response.documents.length > 0
+          ? (response.documents[0] as GymRecordsDocuments)
+          : null
+      ),
       catchError((error) => {
         console.error('Failed loading best gym record:', error);
         return of(null);
       })
     );
   }
-  
-  getBestTimeRecord(userId: string, type: number, distance: number): Observable<RunningAndCyclingRecordsDocuments | null> {
+
+  getBestTimeRecord(
+    userId: string,
+    type: number,
+    distance: number
+  ): Observable<RunningAndCyclingRecordsDocuments | null> {
     if (!userId || !type || !distance) {
       return of(null);
     }
     return from(
-      this.database.listDocuments(this.databaseId, this.runningAndCyclingRecordsId, [
-        Query.equal('user_id', userId),
-        Query.equal('type', type),
-        Query.equal('distance', distance),
-        Query.orderAsc('record_time'),
-        Query.limit(1),
-      ])
+      this.database.listDocuments(
+        this.databaseId,
+        this.runningAndCyclingRecordsId,
+        [
+          Query.equal('user_id', userId),
+          Query.equal('type', type),
+          Query.equal('distance', distance),
+          Query.orderAsc('record_time'),
+          Query.limit(1),
+        ]
+      )
     ).pipe(
-      map((response) => (response.documents.length > 0 ? response.documents[0] as RunningAndCyclingRecordsDocuments : null)),
+      map((response) =>
+        response.documents.length > 0
+          ? (response.documents[0] as RunningAndCyclingRecordsDocuments)
+          : null
+      ),
       catchError((error) => {
         console.error('Failed loading best time record:', error);
         return of(null);
