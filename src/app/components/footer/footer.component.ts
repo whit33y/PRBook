c85f7a864667d2fb7e3d3548e23a95c8a915d19d
whit33y <troppout@gmail.com>
2025-03-04 18:02:35 +0100

    add links to footer + small change on home page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
+  goToGithub() {
+    window.open('https://github.com/whit33y');
+  }
+
+  goToLinkedin() {
+    window.open('https://www.linkedin.com/in/jakub-bryska-b905b0279/');
+  }
 }
